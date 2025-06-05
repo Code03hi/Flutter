04cdfd652202d1e5291cdf5414a3e49199ab8357
Bhagwan jha <analysthacker03@gmail.com>
2025-06-05 09:33:37 +0530

    first commit

---
+++ android/app/src/main/kotlin/com/example/flutter_catalog/MainActivity.kt
@@ -0,0 +1,7 @@
+package com.example.flutter_catalog
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity() {
+
+}
---
+++ lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyWidget());
+}
+
+class MyWidget extends StatelessWidget {
+  const MyWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Material(
+        child: Center(
+          child: Container(
+            child: Text("Hello Sameer"),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
